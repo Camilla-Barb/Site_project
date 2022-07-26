6b3faac1f9be358e135ad63645c7ece6eb89594c
Camilla Barbieri <camilla.barbieri@accenture.com>
2022-07-26 15:20:51 +0200

    solve swiper bug pagination

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-const swiper = new Swiper(".swiper", {});
-
 document.addEventListener("DOMContentLoaded", function () {
   //Dichiaro costante swiper per poterla riutilizzare eventualmente in futuro
 
@@ -15,19 +13,18 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 const selectSize = (e) => {
-  let arrDataSet = [...document.querySelectorAll("[data-size]")];
+  // let arrDataSet = [...document.querySelectorAll("[data-size]")];
   let infoSize = document.querySelector(".size-info");
   infoSize.style.display = "block";
   if (e.currentTarget.dataset.size === "xs") {
     //taglia non disponibile
-
     infoSize.innerHTML = "Taglia non disponibile";
   } else if (e.currentTarget.dataset.size === "s") {
     // ultimo capo disponibile
     infoSize.innerHTML = "Ultimo capo disponibile";
   } else {
     //Vai al carrello
-    infoSize.innerHTML = "Vai al carrello per continuare il tuo acquisto";
+    infoSize.innerHTML = "Aggiungi al carrello per continuare il tuo acquisto";
   }
 };
 
@@ -36,6 +33,35 @@ const checkBtn = () => {
   allSizeBtn.forEach((el) => {
     el.addEventListener("click", selectSize);
   });
+};
+
+let productInfo = {
+  color: "",
+  size: "",
+};
+
+// el.classList.toggle("focus");
+//  if (el.clicked == true) {
+//   el.classList.remove("focus");
+// } else {
+//   el.classList.toggle("focus");
+// }
+
+const addToCart = () => {
+  const btnAddToCart = document.querySelector(".add-to-cart");
+  let infoSize = document.querySelector(".size-info");
+  const chooseColor = [...document.querySelectorAll(".select-color")];
+  if (
+    (infoSize.innerHTML !== "Taglia non disponibile") &
+    chooseColor.classList.contains("focus")
+  ) {
+    //al click del btn => alert("Prodotto aggiunto al carrello")
+    btnAddToCart.addEventListener("click", function () {
+      alert("Prodotto aggiunto al carrello");
+    });
+  } else {
+    alert("error");
+  }
 };
 
 const init = async () => {
